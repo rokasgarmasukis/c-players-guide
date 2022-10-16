b7f819a71af6224d6f080ea6f1dc8e07b6ebce12
Rokas Garmasukis <rokas.garmasukis@gmail.com>
2022-10-16 19:09:54 +0200

    Finish ch13, begin ch14

---
+++ 14_MemoryManagement/HuntingTheManticore/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.Title = "Hunting the Manticore";
+
+int healthManticore = 10;
+int maxHealthManticore = 10;
+int healthCity = 15;
+int maxHealthCity = 15;
+int round = 1;
+
+int distanceManticore = AskForNumberInRange("Player 1, how far away from the city do you want to station the Manticore? ", 0, 100);
+
+Console.Clear();
+
+
+
+
+
+int AskForNumber(string text)
+{
+    Console.WriteLine(text);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+int AskForNumberInRange(string text, int min, int max)
+{
+    while (true)
+    {
+        int number = AskForNumber(text);
+        if (number >= min && number <= max) return number;
+    }
+}
