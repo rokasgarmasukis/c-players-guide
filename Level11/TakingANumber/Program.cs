de6d7a9fa0f56a64e2cc22be916964cdb02d9f8a
Rokas Garmasukis <rokas.garmasukis@gmail.com>
2023-02-09 16:30:02 +0100

    Complete level 11

---
+++ Level11/TakingANumber/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.Title = "Level 11 - Ask For a Number";
+
+
+int AskForNumber(string text)
+{
+    Console.Write(text);
+    int input = Convert.ToInt32(Console.ReadLine());
+    return input;
+}
+
+
+int AskForNumberInRange(string text, int min, int max)
+{
+    int input;
+    while (true)
+    {
+        Console.Write(text);
+        input = Convert.ToInt32(Console.ReadLine());
+        if (input > max || input < min) continue;
+        break;
+    }
+    return input;
+}
+
+int answer = AskForNumber("give me some number: ");
+Console.WriteLine($"Here is it: {answer}");
+
+answer = AskForNumberInRange("give me some number between 5 and 10: ", 5, 10);
+Console.WriteLine($"Here is it: {answer}");
