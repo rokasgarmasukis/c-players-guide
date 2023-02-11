8d9b183316012ac67e0d6333605132a4ea300b3f
Rokas Garmasukis <rokas.garmasukis@gmail.com>
2023-02-11 23:26:51 +0100

    Finish Level 13

---
+++ Level13/CountDown/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Title = "Countdown";
+
+void Countdown(int number)
+{
+    Console.WriteLine(number);
+    if (number == 1) return;
+    else Countdown(number - 1);
+}
+
+Countdown(10);
---
+++ Level13/TakingANumber/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Title = "Taking a Number";
+
+int AskForNumber(string text)
+{
+    Console.Write(text);
+    return int.Parse(Console.ReadLine());
+}
+
+int AskForNumberInRange(string text, int min, int max)
+{
+    int numInRange;
+    while (true)
+    {
+        Console.Write(text);
+        numInRange = int.Parse(Console.ReadLine());
+        if (numInRange < min || numInRange > max) continue;
+        break;
+    }
+    return numInRange;
+}
+
+int someNumber = AskForNumberInRange("Gimme a number between 5 and 10: ", 5, 10);
+Console.WriteLine(someNumber);
