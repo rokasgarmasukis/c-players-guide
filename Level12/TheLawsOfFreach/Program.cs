4c59909d9a276eeedcd5e602ce9175bc2cac787a
Rokas Garmasukis <rokas.garmasukis@gmail.com>
2023-02-11 23:06:11 +0100

    Finish Level 12

---
+++ Level12/TheLawsOfFreach/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Title = "The Laws of Freach";
+
+int[] array = new int[] { 4, 51, -7, 13, -99, 15, -8, 45, 90 };
+
+// calculating minimum
+int currentSmallest = int.MaxValue; // Start higher than anything in the array. 
+foreach (int value in array)
+{
+    if (value < currentSmallest)
+        currentSmallest= value;
+}
+Console.WriteLine(currentSmallest);
+
+// calculating average
+int total = 0;
+foreach (int value in array)
+    total += value;
+float average = (float)total / array.Length;
+Console.WriteLine(average);
---
+++ Level12/TheReplicatorOfDTo/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Title = "The Replicator of D’To";
+
+int[] firstArray = new int[5];
+
+firstArray[0] = int.Parse(Console.ReadLine());
+firstArray[1] = int.Parse(Console.ReadLine());
+firstArray[2] = int.Parse(Console.ReadLine());
+firstArray[3] = int.Parse(Console.ReadLine());
+firstArray[4] = int.Parse(Console.ReadLine());
+
+int[] secondArray = new int[5];
+
+for(int i = 0; i < 5; i++)
+{
+    secondArray[i] = firstArray[i];
+}
+
+for(int i = 0;i < 5; i++)
+{
+    Console.WriteLine($"first Array: {firstArray[i]}; copy: {secondArray[i]}");
+}
