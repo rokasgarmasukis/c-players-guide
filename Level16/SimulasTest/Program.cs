94c96a1fcbdfb6bcca7842f44a9ec84173ea0422
Rokas Garmasukis <rokas.garmasukis@gmail.com>
2023-02-12 16:36:59 +0100

    Finish Level 16

---
+++ Level16/SimulasTest/Program.cs
@@ -0,0 +1,39 @@
+﻿Console.Title = "Simula's Test";
+
+DoorStatus doorStatus = DoorStatus.Locked;
+
+while (true)
+{
+    Console.Write($"The door is {doorStatus.ToString().ToLower()}. What do you want to do? ");
+    doorStatus = GetNewDoorStatus(doorStatus);
+
+}
+
+
+DoorStatus GetNewDoorStatus(DoorStatus doorStatus)
+{
+
+    string input = Console.ReadLine();
+    if (input == "unlock" && doorStatus == DoorStatus.Locked)
+    {
+        return DoorStatus.Unlocked;
+    }
+    else if (input == "lock" && doorStatus == DoorStatus.Unlocked)
+    {
+        return DoorStatus.Locked;
+    }
+    else if (input == "open" && doorStatus == DoorStatus.Unlocked)
+    {
+        return DoorStatus.Open;
+    }
+    else if (input == "close" && doorStatus == DoorStatus.Open)
+    {
+        return DoorStatus.Unlocked;
+    }
+    else
+    {
+        return doorStatus;
+    }
+}
+
+enum DoorStatus { Locked, Unlocked, Open }
