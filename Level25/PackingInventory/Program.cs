d3d92c1e0254a071ad766447fe4ac70f67e31b3a
Rokas Garmasukis <rokas.garmasukis@gmail.com>
2023-02-17 16:06:55 +0100

    Finish Level 26

---
+++ Level25/PackingInventory/Program.cs
@@ -0,0 +1,110 @@
+﻿Console.Title = "Packing Inventory";
+
+
+
+Pack pack = new(10, 10, 10);
+
+
+while (true)
+{
+    string choice = GetInput("Would you like to know the stats or add item (stats, add): ");
+    switch(choice)
+    {
+        case "add":
+            string itemChoice = GetInput("arrow, bow, rope, water, food, sword: ");
+            switch(itemChoice)
+            {
+                case "arrow":
+                    pack.Add(new Arrow());
+                    break;
+                case "bow":
+                    pack.Add(new Bow());
+                    break;
+                case "rope":
+                    pack.Add(new Rope());
+                    break;
+                case "water":
+                    pack.Add(new Water());
+                    break;
+                case "food":
+                    pack.Add(new Food());
+                    break;
+                case "sword":
+                    pack.Add(new Sword());
+                    break;
+                default:
+                    break;
+            }
+            break;
+        case "stats":
+            pack.PrintStats();
+            break;
+        default:
+            break;
+    }
+}
+
+
+string GetInput(string text)
+{
+    Console.Write(text);
+    return Console.ReadLine();
+}
+
+public class Pack
+{
+    private InventoryItem[] _items { get; }
+    public double MaxWeight { get; }
+    public double MaxVolume { get; }
+    public int MaxCount { get; }
+
+    public double CurrentWeight { get; private set; }
+
+    public double CurrentVolume { get; private set; }
+    public int CurrentCount { get; private set; }
+
+    public Pack(int numOfItems, double maxweight, double maxvolume)
+    {
+        MaxCount = numOfItems;
+        MaxWeight = maxweight;
+        MaxVolume = maxvolume;
+        _items = new InventoryItem[numOfItems];
+    }
+
+    public bool Add(InventoryItem item)
+    {
+        if (CurrentVolume + item.Volume > MaxVolume) return false;
+        if (CurrentWeight + item.Weight > MaxWeight) return false;
+        if (CurrentCount == MaxCount) return false;
+
+        _items[CurrentCount] = item;
+        CurrentCount++;
+        CurrentVolume += item.Volume;
+        CurrentWeight += item.Weight;
+
+        return true;
+    }
+
+    public void PrintStats()
+    {
+        Console.WriteLine($"Items: {CurrentCount}/{MaxCount}, Volume: {CurrentVolume}/{MaxVolume}, Weight: {CurrentWeight}/{MaxWeight}");
+    }
+}
+
+
+
+public class InventoryItem
+{
+    public double Weight { get; set; }
+    public double Volume { get; set; }
+
+    public InventoryItem(double weight, double volume) { Weight = weight; Volume = volume; }
+}
+
+public class Arrow : InventoryItem { public Arrow() : base(0.1, 0.05) { } }
+public class Bow : InventoryItem { public Bow() : base(1, 4) { } }
+public class Rope : InventoryItem { public Rope() : base(1, 1.5) { } } 
+public class Water : InventoryItem { public Water() : base(2, 3) { } }
+public class Food : InventoryItem { public Food() : base(1, 0.5) { } }
+public class Sword : InventoryItem { public Sword() : base(5, 3) { } }
+
---
+++ Level26/TheOldRobot/Program.cs
@@ -0,0 +1,113 @@
+﻿Console.Title = "The Old Robot";
+
+Robot robot = new();
+
+for (int i = 0; i < robot.Commands.Length; i++)
+{
+    string input = Console.ReadLine();
+    RobotCommand command = input switch
+    {
+        "on" => new OnCommand(),
+        "off" => new OffCommand(),
+        "north" => new NorthCommand(),
+        "south" => new SouthCommand(),
+        "west" => new WestCommand(),
+        "east" => new WestCommand(),
+    };
+    robot.Commands[i] = command;
+}
+
+robot.Run();
+
+//string[] commands = new string[3];
+//commands[0] = Console.ReadLine();
+//commands[1] = Console.ReadLine();
+//commands[2] = Console.ReadLine();
+
+//for (int i = 0; i < 3; i++)
+//{
+//    switch (commands[i])
+//    {
+//        case "off":
+//            robot.Commands[i] = new OffCommand();
+//            break;
+//        case "on":
+//            robot.Commands[i] = new OnCommand();
+//            break;
+//        case "west":
+//            robot.Commands[i] = new WestCommand();
+//            break;
+//        case "east":
+//            robot.Commands[i] = new EastCommand();
+//            break;
+//        case "north":
+//            robot.Commands[i] = new NorthCommand();
+//            break;
+//        case "south":
+//            robot.Commands[i] = new SouthCommand();
+//            break;
+//        default:
+//            break;
+//    }
+//}
+
+
+
+public class Robot
+{
+    public int X { get; set; }
+    public int Y { get; set; }
+    public bool IsPowered { get; set; }
+    public RobotCommand?[] Commands { get; } = new RobotCommand?[3];
+    public void Run()
+    {
+        foreach (RobotCommand? command in Commands)
+        {
+            command?.Run(this);
+            Console.WriteLine($"[{X} {Y} {IsPowered}]");
+        }
+    }
+
+
+}
+
+public abstract class RobotCommand { public abstract void Run(Robot robot); }
+
+public class OnCommand : RobotCommand
+{
+    public override void Run(Robot robot) => robot.IsPowered = true;
+}
+
+public class OffCommand : RobotCommand
+{
+    public override void Run(Robot robot) => robot.IsPowered = false;
+}
+
+public class NorthCommand : RobotCommand
+{
+    public override void Run(Robot robot)
+    {
+        if (robot.IsPowered) { robot.Y++; }
+    }
+}
+
+public class SouthCommand : RobotCommand
+{
+    public override void Run(Robot robot)
+    {
+        if (robot.IsPowered) { robot.Y--; }
+    }
+}
+
+public class EastCommand : RobotCommand
+{
+    public override void Run(Robot robot)
+    {
+        if (robot.IsPowered) { robot.X++; }
+    }
+}
+
+public class WestCommand : RobotCommand
+{
+    public override void Run(Robot robot) { if (robot.IsPowered) robot.X--; }
+}
