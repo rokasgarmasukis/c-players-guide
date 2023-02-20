fd5633f8056d1bcd37cd69f6302b365d85568720
Rokas Garmasukis <rokas.garmasukis@gmail.com>
2023-02-20 10:40:07 +0100

    Finish Level 28

---
+++ Level28/RoomCoordinates/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.Title = "Room Coordinates";
+
+Coordinate a = new Coordinate(1, 2);
+Coordinate b = new Coordinate(1, 1);
+Coordinate c = new Coordinate(0, 1);
+
+Console.WriteLine(a.IsAdjacent(b));
+Console.WriteLine(a.IsAdjacent(c));
+
+public struct Coordinate
+{
+    public int X { get;  }
+    public int Y { get; }
+
+    public Coordinate(int x, int y)
+    {
+        X = x; Y = y;
+    }
+
+    public bool IsAdjacent(Coordinate other)
+    {
+        if ((Math.Abs(other.X - X) == 1 && other.Y == Y) || (Math.Abs(other.Y - Y) == 1 && other.X == X))
+        {
+            return true;
+        }
+        return false;
+    }
+}
