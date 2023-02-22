b0eaa60c506867d5721b5f02d3aa8997e1ef75f6
Rokas Garmasukis <rokas.garmasukis@gmail.com>
2023-02-22 11:53:08 +0100

    Finish Level 35

---
+++ Level35/ExceptisGame/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Title = "Excepti's Game";
+
+List<int> usedNumbers = new List<int>();
+
+Random random = new Random();
+int gameNumber = random.Next(10);
+
+while(true)
+{
+    Console.Write("Pick a number between 0 and 9: ");
+    int input = Convert.ToInt32(Console.ReadLine());
+    try
+    {
+        if (input == gameNumber) throw new ArgumentException("You cannot choose this number.");
+    }
+    catch (ArgumentException e)
+    {
+        Console.WriteLine(e.Message);
+    }
+    if (usedNumbers.Contains(input)) continue;
+    usedNumbers.Add(input);
+
+}
