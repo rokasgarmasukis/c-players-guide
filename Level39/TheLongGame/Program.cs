7bbd40aa27154e5ce443d19250b7dbb8b62a121e
Rokas Garmasukis <rokas.garmasukis@gmail.com>
2023-02-23 08:55:51 +0100

    Finish Level 39

---
+++ Level39/TheLongGame/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Title = "The Long Game";
+
+Console.Write("What's your name? ");
+string name = Console.ReadLine() ?? "Unknown";
+string fileName = $"{name}.txt";
+
+int playerScore;
+if (File.Exists(fileName))
+    playerScore = Convert.ToInt32(File.ReadAllText(fileName));
+else
+    playerScore = 0;
+
+while (true)
+{
+    ConsoleKey key = Console.ReadKey().Key;
+    if (key == ConsoleKey.Enter)
+        break;
+
+    playerScore++;
+    Console.WriteLine($"Your score: {playerScore}");
+}
+File.WriteAllText(fileName, $"{playerScore}");
