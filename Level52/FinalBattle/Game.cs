97576c52572b5e1446fdef2223c7ec1574ad829f
Rokas Garmasukis <rokas.garmasukis@gmail.com>
2023-02-27 17:34:49 +0100

    Initial commit

---
+++ Level52/FinalBattle/Game.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalBattle
+{
+    internal class Game
+    {
+    }
+}
---
+++ Level52/FinalBattle/Program.cs
@@ -0,0 +1,2 @@
+﻿Console.Title = "Final Battle";
+
